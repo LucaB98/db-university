92e177dab5106b2acb2acb52cb99176bdc5a26de
LucaB98 <lucabellan98@gmail.com>
2024-03-01 14:20:32 +0100

    finish exercise GROUP BY

--- img/soluction.sql
+++ img/soluction.sql
@@ -30,6 +30,13 @@ SELECT * FROM `teachers` WHERE `phone` IS NULL;
 -- GROUP BY
 
 -- 1. Contare quanti iscritti ci sono stati ogni anno
+SELECT COUNT(*), YEAR(`enrolment_date`) FROM `students` GROUP BY YEAR(`enrolment_date`);
+
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+SELECT COUNT(*), `office_address` FROM `teachers` GROUP BY `office_address`;
+
 -- 3. Calcolare la media dei voti di ogni appello d'esame
+SELECT ROUND(AVG(`vote`),0) AS 'media_voti', `exam_id` AS 'esame' FROM `exam_student` GROUP BY `exam_id`;
+
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
+SELECT COUNT(*) AS 'n_corsi', `department_id` AS 'dipartimento' FROM `degrees` GROUP BY `department_id`;
