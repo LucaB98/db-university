48004c1364a77c3099f1d7f64ac13d063a52891b
LucaB98 <lucabellan98@gmail.com>
2024-03-04 15:28:25 +0100

    join 1

--- img/soluction.sql
+++ img/soluction.sql
@@ -33,10 +33,30 @@ SELECT * FROM `teachers` WHERE `phone` IS NULL;
 SELECT COUNT(*), YEAR(`enrolment_date`) FROM `students` GROUP BY YEAR(`enrolment_date`);
 
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
-SELECT COUNT(*), `office_address` FROM `teachers` GROUP BY `office_address`;
+SELECT COUNT(*) AS `numero_insegnati`, `office_address` FROM `teachers` GROUP BY `office_address`;
 
 -- 3. Calcolare la media dei voti di ogni appello d'esame
 SELECT ROUND(AVG(`vote`),0) AS 'media_voti', `exam_id` AS 'esame' FROM `exam_student` GROUP BY `exam_id`;
 
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
 SELECT COUNT(*) AS 'n_corsi', `department_id` AS 'dipartimento' FROM `degrees` GROUP BY `department_id`;
+
+
+-- JOIN
+
+
+-- 1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
+SELECT s.`id`,S.`name`, S.`surname` FROM `students`AS S JOIN `degrees`AS D ON S.`degree_id`= D.`id` WHERE D.`name`= 'Corso di Laurea in Economia';
+
+-- 2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
+
+
+-- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+
+-- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
+
+-- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
+
+-- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
+
+-- 7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per superare ciascuno dei suoi esami
